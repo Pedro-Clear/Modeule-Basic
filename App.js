72e8ef70cbcb5216b33f6001cf3e95e01b0f0f33
Pedro Gabriel <pedrogabriel@MacBook-Air-de-Pedro.local>
2023-08-09 21:34:19 -0300

    Module basic: Flexbox

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Image } from 'react-native'; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// class App extends Component{
+//   render(){
+
+//     let nome = 'Matheus';
+
+//     return(
+//       <View>
+//         <Text style={{ color: '#FF0000', fontSize: 25, marginTop: 35 }}>
+//           Sujeito Programador
+//         </Text>
+
+//         <Text style={{ fontSize: 30 }}> {nome} </Text>
+
+//         <Jobs largura={100} altura={200} fulano="Matheus" />
+
+//       </View>
+//     );
+//   }
+// }
+
+// class Jobs extends Component{
+//   render(){
+
+//     let img = 'https://sujeitoprogramador.com/steve.png';
+
+//     return(
+//       <View>
+//         <Image 
+//           source={{ uri: img }}
+//           style={{ width: this.props.largura, height: this.props.altura }}
+//       />
+//       <Text> {this.props.fulano} </Text>
+//      </View>
+//     );
+//   }
+// }
+
+class App extends Component{
+  render(){
+    return(
+      <View style={{flex:1, backgroundColor: '#222'}}>
+
+          <View style={{height: 65, backgroundColor: '#222'}}></View>
+
+          <View style={{flex:1, backgroundColor: '#FFF'}}></View>
+
+          <View style={{height:65, backgroundColor: '#222'}}></View>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default App;
---
+++ myApp/myApp/myAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myAppApp.swift
+//  myApp
+//
+//  Created by Pedro Gabriel on 08/08/23.
+//
+
+import SwiftUI
+
+@main
+struct myAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
