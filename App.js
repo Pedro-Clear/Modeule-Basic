b38adfc4dbcbe8624afd02f82566b7afe16392ab
Pedro Gabriel <pedrogabriel@MacBook-Air-de-Pedro.local>
2023-08-10 23:46:43 -0300

    InstaSujeito Pt 1

--- App.js
+++ App.js
@@ -1,52 +1,125 @@
+
 import React, { Component } from 'react';
 import {
-  View,
-  Text,
+  Platform,
   StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
   FlatList
-  } from 'react-native';
+} from 'react-native';
 
-import Pessoas from './src/Pessoas';
+import Lista from './src/Lista';
 
-class App extends Component{
+class App extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      lista: [
-        {id: '1', nome:'Matheus', idade:50, email:'matheus@matheus.com'},
-        {id: '2', nome:'Thiago', idade:33, email:'thiago@thiago.com'},
-        {id: '3', nome:'Lucas', idade:20, email:'lucas@lucas.com'},
-        {id: '4', nome:'Henrique', idade:50, email:'Henrique@Henrique.com'},
-        {id: '5', nome:'Thiago', idade:33, email:'thiago@thiago.com'},
-        {id: '6', nome:'Lucas', idade:20, email:'lucas@lucas.com'},
-        {id: '7', nome:'JOSE', idade:33, email:'thiago@thiago.com'},
-        {id: '8', nome:'HENRIQUE', idade:20, email:'lucas@lucas.com'},
-
+      feed: [
+        {
+          id: '1', 
+          nome: 'Lucas Silva', 
+          descricao: 'Mais um dia de muitos bugs :)', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto1.png',  
+          likeada: false, 
+          likers: 0
+         },
+        {
+          id: '2', 
+          nome: 'Matheus', 
+          descricao: 'Isso sim é ser raiz!!!!!', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto2.png', 
+          likeada: false, 
+          likers: 0
+        },
+        {
+          id: '3', 
+          nome: 'Jose Augusto', 
+          descricao: 'Bora trabalhar Haha', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil3.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto3.png',  
+          likeada: false, 
+          likers: 3
+        },
+        {
+          id: '4', 
+          nome: 'Gustavo Henrique', 
+          descricao: 'Isso sim que é TI!', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto4.png', 
+          likeada: false, 
+          likers: 1
+        },
+        {
+          id: '5', 
+          nome: 'Guilherme', 
+          descricao: 'Boa tarde galera do insta...', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto5.png',
+          likeada: false, 
+          likers: 32
+        }
       ]
-    };
+     };
+ 
   }
 
-  render(){
-    return(
-      <View style={styles.container}> 
+  render() {
+    return (
+      <View style={styles.container}>
+      
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Image
+          source={require('./src/img/logo.png')}
+          style={styles.logo}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Image
+          source={require('./src/img/send.png')}
+          style={styles.send}
+          />
+        </TouchableOpacity>
+      </View>
 
       <FlatList
-      data={this.state.lista}
-      keyExtractor={(item)=> item.id}
-      renderItem={ ({item}) => <Pessoas data={item} /> }
+      showsHorizontalScrollIndicator={false}
+      data={this.state.feed}
+      renderItem={ ({item}) => <Lista data={item} /> }
       />
 
-      </View>    
+      </View>
     );
   }
-
 }
+
+
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
   },
+  header:{
+    height: 55,
+    backgroundColor: '#FFF',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 5,
+    borderBottomWidth: 0.2,
+    shadowColor: '#000',
+    elevation: 1,
+  },
+  send:{
+    width: 23,
+    height: 23
+  }
+
 });
 
 export default App;
-
